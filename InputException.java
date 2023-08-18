765cd833496d32e395fc97f7765c16b03f0810cd
IAN Lee <87459518+DonghyeopLee@users.noreply.github.com>
2023-08-18 16:33:08 +1000

    MMS
    all firle

---
+++ InputException.java
@@ -0,0 +1,6 @@
+
+public class InputException extends Exception{      
+    public InputException(String message){
+        super(message);   
+    }   
+}
---
+++ Session.java
@@ -0,0 +1,19 @@
+public class Session 
+{
+    private SuperMarket SuperMarket;
+    private SuperMarkets SuperMarkets;
+    
+    public Session(){
+        this.SuperMarkets = new SuperMarkets(); 
+        this.SuperMarket = new SuperMarket();
+    }
+ 
+    public SuperMarket getSuperMarket(String email, String password){
+        return SuperMarkets.getSuperMarket(email, password);
+    }
+    
+    public void setSuperMarket(SuperMarket e){
+        this.SuperMarket = e;
+    }
+}
+
